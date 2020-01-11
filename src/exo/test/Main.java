b6a2aeaeef9b6a9cfccf7ef5b2bfbc56191dd876
mohkeita <mohkeita13@gmail.com>
2020-01-11 22:31:47 +0100

    test code with main class

--- src/exo/test/Main.java
+++ src/exo/test/Main.java
@@ -14,8 +14,9 @@ public class Main {
 
         HealthyBurger healthyBurger =  new HealthyBurger("Bacon", 5.67);
         healthyBurger.addHamburgerAddition1("Egg", 5.43);
-        healthyBurger.itemizeHamburger();
         healthyBurger.addHealthyAddition1("Lentils", 3.41);
+        System.out.println("Total Healthy Burger price is " +healthyBurger.itemizeHamburger());
+
 
     }
 }
