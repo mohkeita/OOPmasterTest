b0bb4e0d7a453e168276866ac7768fe54cebce87
mohkeita <mohkeita13@gmail.com>
2020-01-11 22:39:56 +0100

    create DeluxeHamburger class and constructor

---
+++ src/exo/test/DeluxeBurger.java
@@ -0,0 +1,10 @@
+package exo.test;
+
+public class DeluxeBurger extends Hamburger {
+
+    public DeluxeBurger() {
+        super("Deluxe", "Sausage & Bacon", 14.54, "White");
+        super.addHamburgerAddition1("Chips", 2.75);
+        super.addHamburgerAddition2("Drink", 1.81);
+    }
+}
